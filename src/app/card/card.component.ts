2415db558cec27ba9cee8c002ec24b3377690d68
Hubert Janczak <huberq@gmail.com>
2022-03-22 21:42:21 +0100

    commit :)

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -7,13 +7,17 @@ import { LoadServiceService } from '../service/load-service.service';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-
+public tableX = [1,2,3,4,5];
   constructor(
     public loadingIndicator: LoadServiceService
   ) { 
 
-    loadingIndicator.Set("K1",false);
-    loadingIndicator.Set("K2",false);
+    // loadingIndicator.Set("K1",false);
+    // loadingIndicator.Set("K2",false);
+    for (var val of this.tableX){
+      loadingIndicator.Set('qq'+val,false);
+    }
+  
   }
 
   ngOnInit(): void {
@@ -21,7 +25,12 @@ export class CardComponent implements OnInit {
   
   doAction(): void{
     this.loadingIndicator.loading.next("dddpa");
-    this.loadingIndicator.Toggle('K1');
-    this.loadingIndicator.Toggle('K2');
+    // this.loadingIndicator.Toggle('K1');
+    // this.loadingIndicator.Toggle('K2');
+    for (var val of this.tableX){
+      this.loadingIndicator.Toggle('qq'+val);
+    }
+
+
   }
 }
--- src/app/card01/card01.component.ts
+++ src/app/card01/card01.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { LoadServiceService } from '../service/load-service.service';
 
 @Component({
@@ -6,11 +6,18 @@ import { LoadServiceService } from '../service/load-service.service';
   templateUrl: './card01.component.html',
   styleUrls: ['./card01.component.css']
 })
-export class Card01Component implements OnInit {
+export class Card01Component implements OnInit,OnDestroy {
+  @Input() public imageSrc: string;
 
   constructor(
     public loadingIndicator: LoadServiceService
-  ) { }
+  ) { 
+    this.imageSrc="nic";
+  }
+  
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
 
   ngOnInit(): void {
   }
